13306ca19ccbcb0ec69d8087bf2b4746d26265a3
cmk276 <116907913+cmk276@users.noreply.github.com>
2023-06-03 21:16:12 +0200

    Modificada Administración de Modelos de alumnos

--- aicolearning/modelos_de_alumnos/admin.py
+++ aicolearning/modelos_de_alumnos/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import DefinicionModelo, Caracteristica, ValorSeleccion
+
+class CaracteristicaInline(admin.TabularInline):
+    model = Caracteristica
+    extra = 3
+
+class DefinicionModeloAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["nombre"]})
+    ]
+
+    inlines = [CaracteristicaInline]
+
+admin.site.register(DefinicionModelo, DefinicionModeloAdmin)
+
+class ValorSeleccionInline(admin.StackedInline):
+    model = ValorSeleccion
+    extra = 3
+
+class DefinicionCaracteristicaAdmin(admin.ModelAdmin):
+    inlines = [ValorSeleccionInline]
+
+admin.site.register(Caracteristica, DefinicionCaracteristicaAdmin)
