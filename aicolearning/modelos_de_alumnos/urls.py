01def4ddb3a00a55ba77971bbd2b76620a539e60
cmk276 <116907913+cmk276@users.noreply.github.com>
2023-06-04 11:39:18 +0200

    Creadas vistas de prueba

--- aicolearning/modelos_de_alumnos/urls.py
+++ aicolearning/modelos_de_alumnos/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = "modelos_de_alumnos"
 urlpatterns = [
+    # ex: /modelos_de_alumnos/
     path("", views.index, name="index"),
+    # ex: /modelos_de_alumnos/1/
+    path("<int:modelo_id>/", views.modelo, name="modelo"),
+    # ex: /modelos_de_alumnos/1/importar
+    path("<int:modelo_id>/importar",views.importar, name="importar")
 ]
--- aicolearning/modelos_de_alumnos/views.py
+++ aicolearning/modelos_de_alumnos/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404,  get_list_or_404, render
 from django.http import HttpResponse
+from .models import DefinicionModelo, Caracteristica
+from django.template import loader
+from django.http import Http404
 
 
 def index(request):
-    return HttpResponse("Estás viendo la vista de Modelos de Alumnos")
+    DefinicionesDeModelos = DefinicionModelo.objects.order_by("nombre")
+    template = loader.get_template("modelos_de_alumnos/index.html")
+    context = {
+        "modelo_list": DefinicionesDeModelos,
+    }
+    
+    return HttpResponse(template.render(context, request))
+
+def importar(request, modelo_id):
+    return render(request, "modelos_de_alumnos/importar.html", {"modelo_id" : modelo_id})
+
+def modelo(request, modelo_id):
+
+    Modelo = get_object_or_404(DefinicionModelo, pk=modelo_id) 
+    #Caracteristicas =  get_list_or_404(Caracteristica,definicion_modelo=modelo_id)
+    
+    Caracteristicas = Caracteristica.objects.filter(definicion_modelo=modelo_id)
+    return render(request, "modelos_de_alumnos/detail.html", {"modelo": Modelo, "caracteristicas": Caracteristicas})
