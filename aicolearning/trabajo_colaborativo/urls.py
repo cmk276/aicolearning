40929663c95969471b17aef245eeda6ded06ae5c
cmk276 <116907913+cmk276@users.noreply.github.com>
2023-06-10 12:36:25 +0200

    Configurar agrupamiento hecho

--- aicolearning/trabajo_colaborativo/urls.py
+++ aicolearning/trabajo_colaborativo/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
+from .trabajo_colaborativo_utiles import TIPOS_AGRUPAMIENTO
 
 from . import views
 
 app_name = "trabajo_colaborativo"
 urlpatterns = [
     path("", views.index, name="index"),
+
+    # path para la configuración de agrupamiento pasando un número de alumnos por grupo
+    # ex /trabajo_colaborativo/configurar_agrupamiento/26/10/2/aleatorio/
+    path("configurar_agrupamiento/<int:id_modelo_alumnos>/<int:num_alumnos>/<int:num_alumnos_por_grupo>/", 
+         views.VistaConfigurarAgrupamiento.as_view(), name="configurar_agrupamiento"),
+
+    path("confirmar_agrupamiento/<int:id_modelo_alumnos>/<int:num_alumnos>/<str:tipo_agrupamiento>",name="confirmar_agrupamiento"),
 ]
