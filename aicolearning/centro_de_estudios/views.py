48fdca2f2447513c37874c45a1e3be3beeb664c2
cmk276 <116907913+cmk276@users.noreply.github.com>
2023-06-17 23:23:55 +0200

    Paginador en la vista de Alumnos

--- aicolearning/centro_de_estudios/views.py
+++ aicolearning/centro_de_estudios/views.py
@@ -4,20 +4,21 @@ from django.views.generic import ListView
 from django.db import models
 from .models import Alumno
 from django.views import generic
+from django.core.paginator import Paginator, EmptyPage
 
 # Clase para gestionar el listado de agrupamientos
 class VistaAlumnos(ListView):
-    model = Alumno
+    paginate_by = 10  # Cantidad de registros por página
+    queryset = Alumno.objects.order_by("apellido1","apellido2","nombre")
     template_name = 'centro_de_estudios/alumnos.html'
     context_object_name = 'alumnos'
+    
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         # Titulo de la página	
         context['titulo'] = "Alumnos"
 
-        # Lista de alumnos
-        context['alumnos'] = Alumno.objects.all().order_by('apellido1', 'apellido2', 'nombre')
         return context
 
 
